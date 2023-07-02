cb6afbcc6ec09966369a90080825f503072a81de
Cleef Souza <a.cleef.souza@gmail.com>
2023-07-02 12:30:25 -0300

    Feat(utils): pacote para constantes e outras ultilidades.

---
+++ src/main/java/com/api/votacoes/utils/ConstantesUtils.java
@@ -0,0 +1,13 @@
+package com.api.votacoes.utils;
+
+public final class ConstantesUtils {
+    public static final String PAUTA_URL = "/pauta";
+
+    public static final String SESSAO_URL = "/sessao";
+
+    public static final String ASSOCIADO_URL = "/associado";
+
+    private ConstantesUtils() {
+        throw new AssertionError();
+    }
+}
